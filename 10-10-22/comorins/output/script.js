e9a5389ee606b4d6594e90ff23bb4586742effe5
Nathika <anithasekar831@gmail.com>
2022-10-18 16:43:57 +0530

    10-10-2022

--- 10-10-22/comorins/output/script.js
+++ 10-10-22/comorins/output/script.js
@@ -9,7 +9,7 @@ let products=[
             id:1,
             img:"https://rukminim1.flixcart.com/image/612/612/knj7wcw0/bottle/v/4/7/1000-thermosteel-1-fg-tms-fis-0062-milton-original-imag276hgtk7scwj.jpeg?q=70",
             p_name:"Pigeon Stark Therminox 1000 ml Flask",
-            price:"₹383",
+            price:"383",
             size:"capacity:100ml",
             Delivery:"free delivery"
         },
@@ -17,7 +17,7 @@ let products=[
             id:2,
             img:"https://rukminim1.flixcart.com/image/416/416/xif0q/bottle/g/c/p/-original-imaggyyky9psjug9.jpeg?q=70",
             p_name:"MILTON Thermosteel Flip lid 1000 ml Flask ",
-            price:"₹450",
+            price:"450",
             size:"capacity:100ml",
             Delivery:"free delivery"
         },
@@ -25,7 +25,7 @@ let products=[
             id:3,
             img:"https://rukminim1.flixcart.com/image/612/612/l3es13k0/bottle/i/y/a/1000-unbreakable-silicone-water-bottle-1-liters-with-original-imagejg6vgqcz6pa.jpeg?q=70",
             p_name:"Sunvibe Motivational Water Bottle",
-            price:"₹ 1500",
+            price:" 1500",
             size:"capacity:100ml",
             Delivery:"free delivery"
         },
@@ -33,7 +33,7 @@ let products=[
           id:4,
           img:"https://rukminim1.flixcart.com/image/416/416/kcw9w280/bottle/r/7/h/1000-jc-peacock-printed-copper-bottlebottle-1000-ml-peacock-jc-original-imaftx63zeqysjch.jpeg?q=70",
           p_name:"vedanshstore Vedansh 100% Copper Water Bottle",
-          price:"₹397",
+          price:"397",
           size:"capacity:100ml",
           Delivery:"free delivery"
       },
@@ -41,7 +41,7 @@ let products=[
         id:5,
         img:"https://rukminim1.flixcart.com/image/416/416/xif0q/water-bottle/k/c/v/600-nw-61-dad-i-love-you-so-much-printed-1-black-mug-coaster-original-imaggpqhazch8ht3.jpeg?q=70",
         p_name:"TrendoPrint Bottle, Mug Gift Set",
-        price:"₹500",
+        price:"500",
         size:"capacity:100ml",
         Delivery:"free delivery"
     }
@@ -56,7 +56,7 @@ let products=[
             id:1,
             img:"https://rukminim1.flixcart.com/image/495/594/kwdv3bk0/kurta/d/d/n/m-863-blue-jaffry-embroidery-original-imag92nn6c2ukk77.jpeg?q=50",
             p_name:"Women Floral Print Viscose Rayon Gown Kurta  (Pink)",
-            price:"₹500",
+            price:"500",
             size:"Size : M ,L ,XL ,XXL",
             Delivery:"free delivery"
         },
@@ -64,7 +64,7 @@ let products=[
             id:2,
             img:"https://rukminim1.flixcart.com/image/495/594/kflftzk0/dress/n/c/d/l-one-piece-orrange-line-maa-fab-original-imafwyy3esacjzau.jpeg?q=50",
             p_name:"Women Kurta and Pant Set Viscose Rayon",
-            price:"₹450",
+            price:"450",
            size:"Size : M ,L ,XL ,XXL",
             Delivery:"free delivery"
         },
@@ -72,7 +72,7 @@ let products=[
             id:3,
             img:"https://rukminim1.flixcart.com/image/495/594/xif0q/dress/i/d/z/m-dv320-daevish-original-imag6fxn7fnrggra-bb.jpeg?q=50",
             p_name:"Women Kurta and Pant Set Viscose Rayon",
-            price:"₹1500",
+            price:"1500",
            size:"Size : M ,L ,XL ,XXL",
             Delivery:"free delivery"
         },
@@ -80,7 +80,7 @@ let products=[
               id:4,
               img:"https://rukminim1.flixcart.com/image/832/832/l09w8sw0/kurta/t/d/w/l-raaka-black-grey-kurta-218-raaka-original-imagc3pb2ezgshqy.jpeg?q=70",
               p_name:"Women Solid Viscose Rayon Anarkali Kurta  (Black)",
-              price:"₹500",
+              price:"500",
              size:"Size : M ,L ,XL ,XXL",
               Delivery:"free delivery"
           },
@@ -88,7 +88,7 @@ let products=[
             id:5,
             img:"https://rukminim1.flixcart.com/image/495/594/xif0q/kurta/x/s/i/xs-anarkali-printed-kurti-nanera-original-imagg7nnmggbrjgk.jpeg?q=50",
             p_name:"Women Printed Cotton Rayon Anarkali Kurta  (Pink)",
-            price:"₹500",
+            price:"500",
            size:"Size : M ,L ,XL ,XXL",
             Delivery:"free delivery"
         }
@@ -101,33 +101,33 @@ let products=[
      productMenu:[  
         {
             id:1,
-            img:"https://rukminim1.flixcart.com/image/495/594/ktizdzk0/shoe/y/b/x/7-ws-9310-tying-grey-original-imag6ut3hzm2zyqm.jpeg?q=50",
-           p_name:"Thar-01 Blue Sneakers,Sports,Training,Gym,Walking,Stylish For Men  (Blue, Orange)",
-            price:"₹500",
+            img:"https://rukminim1.flixcart.com/image/495/594/kuef2q80/shoe/5/u/l/8-6g-842-8-campus-bt-grn-blk-original-imag7j7tqky8c9rg.jpeg?q=50",
+           p_name:"Thar-01 Blue Sneakers Sports Training Walking Stylish For Men",
+            price:"500",
             size:"Size : 6,7,8,9",
             Delivery:"free delivery"
         },
         {
             id:2,
             img:"https://rukminim1.flixcart.com/image/495/594/xif0q/shoe/f/p/n/-original-imagg4zgaacqnv8e.jpeg?q=50",
-           p_name:"Express-08 Grey Sports,Walking,Running,Gym Running Shoes For Men  (Grey, Green)",
-            price:"₹450",
+           p_name:"Express-08 Grey Sports Walking Running Gym Running Shoes For Men ",
+            price:"450",
             size:"Size : 6,7,8,9",
             Delivery:"free delivery"
         },
         {
             id:3,
             img:"https://rukminim1.flixcart.com/image/495/594/kr83ukw0/shoe/f/l/s/6-95489-40-robbie-jones-black-original-imag52e4z66wsjhz.jpeg?q=50",
-           p_name:"SOCKS Training & Gym Shoes For Men  (Blue)",
-            price:"₹1500",
+           p_name:"SOCKS Training,Gym,Training & stylish Shoes For Men  (Blue)",
+            price:"1500",
             size:"Size : 6,7,8,9",
             Delivery:"free delivery"
         },
         {
           id:4,
           img:"https://rukminim1.flixcart.com/image/495/594/kt8zb0w0/shoe/2/o/e/6-socks-04-grey-6-density-grey-original-imag6n4yzbnfzmgc.jpeg?q=50",
-         p_name:"Men 9325 Latest Collection Stylish Sports Sneakers Running Shoes Running Shoes For Men  (Red, Black)",
-          price:"₹500",
+         p_name:"Latest Collection Stylish Sports Sneakers Running Shoes For Men",
+          price:"500",
           size:"Size : 6,7,8,9",
           Delivery:"free delivery"
       },
@@ -135,7 +135,7 @@ let products=[
         id:5,
         img:"https://rukminim1.flixcart.com/image/495/594/xif0q/shoe/q/b/m/6-mrj1241-aadi-black-original-imag7egwjqgvrhd8-bb.jpeg?q=50",
        p_name:"Boots For Men  (Black)",
-        price:"₹399",
+        price:"399",
         size:"Size : 6,7,8,9",
         Delivery:"free delivery"
     }
@@ -150,7 +150,7 @@ let products=[
             id:1,
             img:"https://rukminim1.flixcart.com/image/495/594/kchzhjk0/watch/d/i/8/d-d-c19-black-piraso-original-imafthxwgqdqkftf.jpeg?q=50",
            p_name:"FULL BLACK FANCY DIAL SPORT PU BELT WATCH",
-            price:"₹500",
+            price:"500",
             size:"6,7,8,9",
             Delivery:"free delivery"
         },
@@ -158,15 +158,15 @@ let products=[
             id:2,
             img:"https://rukminim1.flixcart.com/image/495/594/kwv0djk0/watch/7/8/r/1-anlg-428-blue-blue-21-wayfarer-analogue-men-original-imag9g88dhdzpsbv.jpeg?q=50",
            p_name:"ANLG-428-BLUE-BLUE-21-WAYFARER ",
-            price:"₹450",
+            price:"450",
             size:"6,7,8,9",
             Delivery:"free delivery"
         },
         {
             id:3,
             img:"https://rukminim1.flixcart.com/image/495/594/kihqz680-0/watch/q/c/p/silver-dial-analog-watch-ht-gr811-blk-ch-hybrid-watch-for-men-original-imafy9szgh9vkrp7.jpeg?q=50",
-           p_name:"LED-SQ YOUTH CLUB- LED-SQ DIGITAL WATCH FOR GIRLS & BOYS Digital Watch",
-            price:"₹1500",
+           p_name:"LED-SQ YOUTH CLUB- LED-SQ DIGITAL WATCH",
+            price:"1500",
             size:"6,7,8,9",
             Delivery:"free delivery"
         },
@@ -174,14 +174,14 @@ let products=[
           id:4,
           img:"https://rukminim1.flixcart.com/image/495/594/k51cpe80pkrrdj/watch-refurbished/f/s/j/c-1164-br-fogg-original-imafnv6aftxgwjcs.jpeg?q=50",
          p_name:"TM292G Analog Watch - For Men",
-          price:"₹500",
+          price:"500",
           Delivery:"free delivery"
       },
       {
         id:5,
         img:"https://rukminim1.flixcart.com/image/495/594/ko62xzk0/watch/n/l/t/7071-golden-hmtr-original-imag2zh3rrsbwyee.jpeg?q=50",
        p_name:"7071-GOLDEN GOLD PLATED LONG LIFE DAY N DATE Analog Watch - For Men",
-        price:"₹1500",
+        price:"1500",
         size:"6,7,8,9",
         Delivery:"free delivery"
     }
@@ -194,25 +194,25 @@ let products=[
      productMenu:[  
         {
             id:1,
-            img:"https://rukminim1.flixcart.com/image/312/312/ktbu6q80/mobile/x/w/y/8i-rmx3151-realme-original-imag6zhnabagtrzu.jpeg?q=70",
-           p_name:"SAMSUNG Galaxy F13 (Waterfall Blue, 64 GB)  (4 GB RAM)",
-            price:"₹500",
-            size:"6,7,8,9",
+            img:"https://rukminim1.flixcart.com/image/312/312/l0tweq80/mobile/x/f/u/-original-imagcgtghym8theg.jpeg?q=70",
+           p_name:"SAMSUNG Galaxy F13 (Waterfall Blue, 64 GB)",
+            price:"500",
+            size:"4GB RAM",
             Delivery:"free delivery"
         },
         {
             id:2,
             img:"https://rukminim1.flixcart.com/image/312/312/l0zm64w0/mobile/4/p/u/d41-d41blue-iair-original-imagcnzqyguf6h5f.jpeg?q=70",
            p_name:"IAIR D41  (Blue)",
-            price:"₹450",
-            size:"6,7,8,9",
+            price:"450",
+            size:"4GB RAM",
             Delivery:"free delivery"
         },
         {
             id:3,
             img:"https://rukminim1.flixcart.com/image/312/312/l4ln8nk0/mobile/4/8/h/-original-imagfggrvjdqsrtx.jpeg?q=70",
            p_name:"realme 8i (Space Black, 64 GB)  (4 GB RAM)",
-            price:"₹1500",
+            price:"1500",
             size:"6,7,8,9",
             Delivery:"free delivery"
         },
@@ -220,15 +220,16 @@ let products=[
           id:4,
           img:"https://rukminim1.flixcart.com/image/312/312/l4ln8nk0/mobile/4/8/h/-original-imagfggrvjdqsrtx.jpeg?q=70",
          p_name:"realme C30 (Lake Blue, 32 GB)  (2 GB RAM)",
-          price:"₹500",
+          price:"500",
+          size:"4GB RAM",
           Delivery:"free delivery"
       },
       {
         id:5,
         img:"https://rukminim1.flixcart.com/image/312/312/l4n2oi80/mobile/x/o/a/-original-imagfhu75eupxyft.jpeg?q=70",
        p_name:"SAMSUNG Galaxy F13 (Waterfall Blue, 64 GB)  (4 GB RAM)",
-        price:"₹500",
-        size:"6,7,8,9",
+        price:"500",
+        size:"4GB RAM",
         Delivery:"free delivery"
     }
     ]
@@ -241,32 +242,32 @@ let products=[
         {
             id:1,
             img:"https://rukminim1.flixcart.com/image/612/612/xif0q/dining-table/i/v/m/127-6-seater-1399-794-particle-board-dock-fd01-pengu-762-brown-original-imaggvwyyek5mphm.jpeg?q=70",
-           p_name:"Pengu Engineered Wood 6 Seater Dining Table  (Finish Color - Brown, Knock Down)",
-            price:"₹500",
+           p_name:"Pengu Engineered Wood 6 Seater Dining Table (Finish Color - Brown, Knock Down)",
+            price:"500",
             size:"6,7,8,9",
             Delivery:"free delivery"
         },
         {
             id:2,
             img:"https://rukminim1.flixcart.com/image/416/416/l1ch4sw0/dining-table/s/9/w/1447-8-6-seater-0-rosewood-sheesham-mh-diningtb-d19-divine-arts-original-imagcxzbxbcqk4re.jpeg?q=70",
-           p_name:"Divine Arts Sheesham Wood Set with 6 Chairs for Dining Room Solid Wood 6 Seater Dining Table ",
-            price:"₹450",
+           p_name:"Divine Arts Sheesham Wood Set with 6 Chairs for Dining Room  ",
+            price:"450",
             size:"6,7,8,9",
             Delivery:"free delivery"
         },
         {
             id:3,
             img:"https://rukminim1.flixcart.com/image/612/612/l3j2cnk0/dining-table/0/o/l/1500-6-seater-0-mdf-56101543sd00132-godrej-interio-750-walnut-original-imagemp2ngghctnp.jpeg?q=70",
-           p_name:"Godrej Interio Toast Engineered Wood 6 Seater Dining Table  (Finish Color - Walnut, Knock Down)",
-            price:"₹1500",
+           p_name:"Godrej Interio Toast Engineered Wood 6 Seater Dining Table ",
+            price:"1500",
             size:"6,7,8,9",
             Delivery:"free delivery"
         },
         {
           id:4,
           img:"https://rukminim1.flixcart.com/image/416/416/kziqvm80/dining-table/s/g/v/1135-4-seater-1135-pp-maharaja12-aamrose-705-brown-original-imagbhs47waccmp6.jpeg?q=70",
-         p_name:"Godrej Interio Toast Engineered Wood 6 Seater Dining Table  (Finish Color - Walnut, Knock Down)",
-          price:"₹500",
+         p_name:"Godrej Interio Toast Engineered Wood 6 Seater Dining Table ",
+          price:"500",
           size:"6,7,8,9",
           Delivery:"free delivery"
       },
@@ -274,7 +275,7 @@ let products=[
         id:5,
         img:"https://rukminim1.flixcart.com/image/612/612/l3j2cnk0/dining-table/o/s/k/1600-6-seater-0-tempered-glass-56101543sd00145-godrej-interio-original-imagemp3yz6zwtbh.jpeg?q=70",
        p_name:"TrendoPrint Bottle, Mug Gift Set",
-        price:"₹500",
+        price:"500",
         size:"6,7,8,9",
         Delivery:"free delivery"
     }
@@ -284,34 +285,31 @@ let products=[
   ]
   
         
-if(! localStorage.getItem("flipart")){
+if(! localStorage.getItem("flipkart")){
   localStorage.setItem("flipkart",JSON.stringify(products))
   }
 
   
 
-function returnCards(products) {
-    return "<div class=\"products-cards\">" +products.map(title=> `
-    <div class=\"card-item\ text-center col-3">
+function returnCards() {
+  let product_section="";
+    for(let i=0; i<products.length; i++){
+      product_section+=  ` <div class=\"card-item\ text-center col-3">
    
       <div class="product-header">
-      <a href="view.html?product_name=${title.product_name}"><img src="${title.img}"/></a>
+      <a href="view.html?product_name=${products[i].product_name}"><img src="${products[i].img}"/></a>
       </div>
       <div class="product-content">
-        <h4>${title.product_name}</h4>
-        <p>${title.brand}</p>
+        <h4>${products[i].product_name}</h4>
+        <p>${products[i].brand}</p>
       </div> 
 
-    </div>`).join('') + "</div>";
-  }
+    </div>`
   
-   product_section.innerHTML = returnCards(products);
-
-  // function search(){
-  //   let allItem=document.getElementById("tofind").value;
-  //   let pro=localStorage.getItem("flipkart");
-  //   console.log(pro);
-    
-  // }
+    } document.getElementById("product_section").innerHTML=product_section;
+ 
+  }
+  returnCards();
+  
   
 
--- 10-10-22/comorins/output/view.js
+++ 10-10-22/comorins/output/view.js
@@ -3,13 +3,13 @@
     const urlParams= new URLSearchParams(queryString);
     const selected_product= urlParams.get('product_name');
     // alert(selected_product);
- 
+ let items=[];
     let clicked_productMenu=JSON.parse(localStorage.getItem("flipkart"));
     // console.log(clicked_productMenu[0].productMenu[0]);
-    // console.log(clicked_productMenu);
+     console.log(clicked_productMenu);
     for(let i=0;i<clicked_productMenu.length;i++){
         if(selected_product==clicked_productMenu[i].product_name){
-            let items=clicked_productMenu[i].productMenu;
+            items=clicked_productMenu[i].productMenu;
             let element="";
             for(let j=0;j<items.length;j++){
                 element+=`<div class="col-3" style="max-width: 540px;">
@@ -22,7 +22,7 @@
                       <p class="card-text">${items[j].size}</p>
                       <p class="card-text"><small class="text-muted">${items[j].Delivery}</small></p>
                     </div>
-                    <button type="button" id="add" class="btn btn-success onclick="add()"><a href="kart.html">Add To Cart</a></button>
+                    <a href="kart.html"><button type="button" id="add" class="btn btn-success" onclick="add(${j})">Add To Cart</button></a>
                   </div>
                   <div>
 
@@ -33,19 +33,18 @@
         
         }
     }
-
+      
+   
     if(!localStorage.getItem("addtocart")){
       localStorage.setItem("addtocart",JSON.stringify([]));
   }
    let addproduct=JSON.parse(localStorage.getItem("addtocart"));
-  // let news_arr=JSON.parse(localStorage.getItem("newspaper"));
-  function add(){
-    for(let i=0;i<clicked_productMenu.length;i++){
+ 
+  function add(p){
+  
+          addproduct.push(items[p]);
+        
       
-      if(selected_product==clicked_productMenu[i].product_name){
-          addproduct.push(clicked_productMenu[i]);
-        }
-      }
       localStorage.setItem("addtocart",JSON.stringify(addproduct));
 
     }
@@ -53,17 +52,46 @@
     
     function display(){
       let cart=JSON.parse(localStorage.getItem("addtocart"));
-      let element=" ";
+      let element="";
+      let total=0;
+  
+      console.log(cart)
       for(let i=0;i<cart.length;i++){
-      element+='<div class="card mb-3  col-6" ><img src='+cart[i].image+'><div class="card-body"><h5 class="card-title"><a href="view.html?id='+cart[i].p_name+' "target=_blank">'+cart[i].price+'</a></h5><p class="card-text"><small class="text-muted">'+cart[i].size+'</small></p><button onclick="remove('+i+');">DELETE</button></div></div>';
-                   
-  }  
-  document.getElementById("cartDiv").innerHTML=element;  
+        
+        
+      //  console.log(menus)
+     
+     // element+="<div class='card mb-3  col-6' ><img src='"+cart[i].img+"'><div class='card-body'><h5 class='card-title'>"+cart[i].p_name+"</h5><h5>"+cart[i].price+"</h5><p class='card-text'><small class='text-muted'>"+cart[i].size+"</small></p><button onclick='remove("+i+")'>Remove from cart </button></div></div>";
+      element+= "<div class='card' style='width: 18rem;'> <img class='kartimg card-img-top' src='"+cart[i].img+"' alt='Card image cap'><div class='card-body'><button type='button' onclick='remove("+i+")' class='click btn btn-danger'>Remove</button><a href='placeorder.html' class='click btn btn-success'>place order</a></div></div>";
+      //element+= "<div class='card mb-3' style='max-width: 540px;'><div class='row no-gutters'> <div class='col-md-4'><img src='"+cart[i].img+"'><button type='button' onclick='remove("+i+")' class='click btn btn-danger'>Remove</button><a href='placeorder.html' class='click btn btn-success'>place order</a></div><div class='col-md-8'><div class='card-body'>"
+      total+=parseInt(cart[i].price)
+    }  console.log(element);
+    document.getElementById("cartDiv").innerHTML=element;
+    //console.log(total);
+   document.getElementById("tot").innerHTML=total;
   }
-   display();
+  display();
   function remove(i){
   let deleteitem=JSON.parse(localStorage.getItem("addtocart"));
   deleteitem.splice(i,1);
   localStorage.setItem("addtocart",JSON.stringify(deleteitem));
-  display();
+ display();
   }
+
+
+//   <div class="card mb-3" style="max-width: 540px;">
+//   <div class="row no-gutters">
+//     <div class="col-md-4">
+//       <img src='"+cart[i].img+"'>
+//     </div>
+//     <div class="col-md-8">
+//       <div class="card-body">
+//         <h5 class="card-title">Card title</h5>
+//         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+//         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+  
+  
